2fc9a30e8e1e1a9ca30aa6f7415fb5905e40b1b1
ping huang <ping.huang@dxc.com>
2022-05-10 11:54:21 +0800

    经销商协议bug

--- src/app/DIIGT/pre-book/prebase-info/prebase-info.component.ts
+++ src/app/DIIGT/pre-book/prebase-info/prebase-info.component.ts
@@ -672,7 +672,7 @@ export class PrebaseInfoComponent implements OnInit {
         this.dealList = rest.data;
         let dealerAgreementNo = this.dataBase.agreementNo;
         let select = this.dealList.find(val => dealerAgreementNo == val.agreementNo);
-        !select && (this.dataBase.agreementNo = null);
+        // !select && (this.dataBase.agreementNo = null);
       })
     }
   }
