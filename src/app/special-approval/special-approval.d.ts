b363c222336e9a4150765faa98c47488d4037006
qyuan4 <13197171236@163.com>
2022-06-10 17:53:05 +0800

    待办、已办列表中申请提交时间显示为instance创建时间

--- src/app/special-approval/special-approval.d.ts
+++ src/app/special-approval/special-approval.d.ts
@@ -25,9 +25,10 @@ interface RequestItem {
   processStatus: string; // 流程状态
   status: number; // 是否取消
   spProcNodeName; // 当前节点名称
-  applyId: string; 
-  taskInstId: string; 
+  applyId: string;
+  taskInstId: string;
   procInstId: string;
+  submitTime: string;
 }
 
 export {
