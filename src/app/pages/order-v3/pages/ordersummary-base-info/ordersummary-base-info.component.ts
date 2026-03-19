915d69995d53514c35798e0f04969d7eb538d069
Ham <partner.huang.ham@philips.com>
2026-03-19 19:39:54 +0800

    FETR1608191
    commit 3760a45dabbe5c1fd509e4a21132364d1c147637
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Mon Mar 9 14:16:49 2026 +0800
    
        FETR1608191-调整OIT预付款分期申请号 wording
    
    commit 0b0d3d9cde8bc41ae21f2c4b48b6b2b84942acd2
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Mon Mar 9 14:08:08 2026 +0800
    
        FETR1608191-调整OIT预付款分期申请号 wording
    
    commit 15c459a20f8be1f97628386cac42e302ae57de7e
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Fri Mar 6 14:34:49 2026 +0800
    
        FETR1608191
    
    commit 3e7b1a01b0a2bf91b29dc9069c6ba41e9162be84
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Fri Feb 13 19:36:50 2026 +0800
    
        FETR1608191
    
    commit 786fb79d83bbc83beb7ad98572739570b2c2dd1d
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Fri Feb 13 17:38:55 2026 +0800
    
        FETR1608191
    
    commit 2a69dae186ae4e7674dd60b73005589c9376bde9
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Fri Feb 13 16:00:59 2026 +0800
    
        FETR1608191
    
    commit 45bb1f5a984439e61b18fbe7cae299f7013f4c08
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Thu Feb 12 14:49:51 2026 +0800
    
        FETR1608191
    
    commit 1e2241354dfaccfa483105fd6eeb3bebf7ca4b3c
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Thu Feb 12 10:01:27 2026 +0800
    
        FETR1608191
    
    commit 32498c298852ae2105f34148560418765714f154
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Wed Feb 11 21:48:23 2026 +0800
    
        FETR1608191
    
    commit c46c517ce2ea3a3e2ec7b03c7b20cf1cdc21c718
    Author: Ham <partner.huang.ham@philips.com>
    Date:   Wed Feb 11 20:12:46 2026 +0800
    
        FETR1608191

--- src/app/pages/order-v3/pages/ordersummary-base-info/ordersummary-base-info.component.ts
+++ src/app/pages/order-v3/pages/ordersummary-base-info/ordersummary-base-info.component.ts
@@ -325,7 +325,10 @@ export class OrdersummaryBaseInfoComponent implements OnInit {
     dealerRequestLetterFile: [{ value: [], disabled: !this.editOther }],//cpcl文件
     otherSupportFile: [{ value: [], disabled: !this.editOther }],// 其它支持文件
     cpclFile: [{ value: [], disabled: !this.editOther }],//cpcl文件
-    cpclFileFlag: [null] //CPCL文件是否已查
+    cpclFileFlag: [null], //CPCL文件是否已查
+
+    prepayReferenceNo: [{ value: null, disabled: true }],//OIT预付款分期申请号
+    prepayId: [{ value: null, disabled: true }],//OIT预付款id
   }
   oaAddInfo = {
     customerRequestLetterDate: [null], // 客户要货函日期
@@ -599,7 +602,10 @@ export class OrdersummaryBaseInfoComponent implements OnInit {
       profitGrossRate,
       profitGross,
       dealerProfit,
-      biddingCurrency
+      biddingCurrency,
+
+      prepayId,
+      prepayReferenceNo
     } = contractInfo
     this.formValue.patchValue({
       applyId: data.applyId ? data.applyId : this.applyId,
@@ -705,6 +711,8 @@ export class OrdersummaryBaseInfoComponent implements OnInit {
     })
     this.baseInfoTableData.patchValue({
       ...termsCheckInfo,
+      prepayId,
+      prepayReferenceNo,
       magneticResonanceShieldingFile: termsCheckInfo.magneticResonanceShieldingFile,
       igtThirdPartyFile: termsCheckInfo.igtThirdPartyFile,
     })
@@ -1358,6 +1366,8 @@ export class OrdersummaryBaseInfoComponent implements OnInit {
       paymentProvision: [{ value: val.paymentProvision ? val.paymentProvision : '0', disabled: this.editBase }],//付款条款
       paymentProvisionFile: val.paymentProvisionFile ? [[...val.paymentProvisionFile]] : [],//付款条款文件
       paymentProvisionRemarks: [{ value: val.paymentProvisionRemarks ? val.paymentProvisionRemarks : null, disabled: true }],//付款条款备注
+      prepayReferenceNo: [{ value: val.prepayReferenceNo ? val.prepayReferenceNo : null, disabled: true }],//OIT预付款分期申请号
+      prepayId: [{ value: val.prepayId ? val.prepayId : null, disabled: true }],//OIT预付款ID
       performanceBond: [{ value: val.performanceBond ? val.performanceBond : '0', disabled: true }], //履约保函
       performanceBondFile: val.performanceBondFile ? [[...val.performanceBondFile]] : [],//履约保函文件
       performanceBondRemarks: [{ value: val.performanceBondRemarks ? val.performanceBondRemarks : null, disabled: true }],//履约保函备注
